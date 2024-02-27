2abbca4dad80a4581fac746288564a40dc724921
Eugene Maikov <61806051+Eugene-Maikov@users.noreply.github.com>
2024-02-27 21:19:14 +0300

    init project + начало верстки

---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавит!')
+}
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      :price="1205"
+      :isFavorite="true"
+      :isAdded="true"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
