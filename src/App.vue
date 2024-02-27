fdd7724a3d24eb9530f1fd0a65b83760ce017596
Eugene Maikov <61806051+Eugene-Maikov@users.noreply.github.com>
2024-02-27 23:15:16 +0300

    feat: верстка поиска и сортировки

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import CardList from './components/CardList.vue'
-import Drawer from './components/Drawer.vue'
 import Header from './components/Header.vue'
 </script>
 
@@ -10,7 +9,26 @@ import Header from './components/Header.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
+            <input
+              class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
